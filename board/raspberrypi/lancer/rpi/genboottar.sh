a87e9fd9bd8180b2266e9eebb6f1910062dcce90
James Martinez <james-martinez@users.noreply.github.com>
2018-07-12 11:56:48 -0500

    fixed getting directory name

--- board/raspberrypi/lancer/rpi/genboottar.sh
+++ board/raspberrypi/lancer/rpi/genboottar.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
-BOARD_DIR="$(dirname $0)"
+
+CUR_DIR=$(cd `dirname $0` && pwd)
 echo "Creating boot tar"
 tar -cf $BINARIES_DIR/boot.tar  --numeric-owner \
--C  $BINARIES_DIR zImage bcm2710-rpi-3-b.dtb bcm2710-rpi-3-b-plus.dtb  bcm2710-rpi-cm3.dtb \
+-C  $BINARIES_DIR zImage bcm2710-rpi-3-b.dtb bcm2710-rpi-3-b-plus.dtb \
 -C  $BINARIES_DIR/rpi-firmware bootcode.bin  start.elf fixup.dat overlays/ \
--C  /home/lancer/Desktop/sources/buildroot-2018.05/buildroot/board/raspberrypi/lancer/rpi cmdline.txt config.txt
+-C  $CUR_DIR cmdline.txt config.txt
