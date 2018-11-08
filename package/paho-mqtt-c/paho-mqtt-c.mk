b4974cf5e2ebd0e9c1c19d0ac0b75dc015634801
James <james-martinez@users.noreply.github.com>
2018-11-08 10:11:57 -0600

    added paho

---
+++ package/paho-mqtt-c/paho-mqtt-c.mk
@@ -0,0 +1,33 @@
+################################################################################
+#
+# paho-mqtt-c
+#
+################################################################################
+
+PAHO_MQTT_C_VERSION = v1.3.0
+PAHO_MQTT_C_SITE = $(call github,eclipse,paho.mqtt.c,$(PAHO_MQTT_C_VERSION))
+PAHO_MQTT_C_LICENSE = EPL-1.0 or BSD-3-Clause
+PAHO_MQTT_C_LICENSE_FILES = epl-v10 edl-v10
+PAHO_MQTT_C_INSTALL_STAGING = YES
+
+ifeq ($(BR2_PACKAGE_OPENSSL),y)
+PAHO_MQTT_C_DEPENDENCIES += openssl
+PAHO_MQTT_C_CONF_OPTS += -DPAHO_WITH_SSL=TRUE
+else
+PAHO_MQTT_C_CONF_OPTS += -DPAHO_WITH_SSL=FALSE
+endif
+
+define PAHO_MQTT_C_BUILD_CMDS
+     $(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D) $(MQTTLIB_C_TARGET)
+ endef
+
+#define PAHO_MQTT_C_INSTALL_STAGING_CMDS
+ #     $(INSTALL) -D -m 0755 $(@D)/libfoo.a $(STAGING_DIR)/usr/lib/libfoo.a
+#endef
+
+define PAHO_MQTT_C_INSTALL_TARGET_CMDS
+      $(INSTALL) -D -m 0755 $(@D)/build/output/libpaho-*.so* $(TARGET_DIR)/usr/lib
+ endef
+
+
+$(eval $(generic-package))
