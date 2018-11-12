83f41bed1e5d048172c85a9822e24a01aff9cebb
James <james-martinez@users.noreply.github.com>
2018-11-12 11:48:08 -0600

    openc ct works, need to remove doumentation

--- package/openct/openct.mk
+++ package/openct/openct.mk
@@ -3,12 +3,14 @@
 # openct-0.6.20
 ################################################################################
 
-OPENCT_VERSION =openct-0.6.20
-OPENCT_SITE = $(call github,OpenSC,openct,$(OPENCT_VERSION))
+OPENCT_VERSION =0.6.20
+OPENCT_SITE = $(call github,OpenSC,openct,openct-$(OPENCT_VERSION))
 OPENCT_LICENSE =LGPL-2.1
 OPENCT_LICENSE_FILES = OPENCT/LGPL-2.1
-OPENCT_DEPENDENCIES = host-pkgconf host-libtool
+OPENCT_DEPENDENCIES = host-pkgconf libtool libusb-compat pcsc-lite
+OPENCT_CONF_OPT +=  --enable-usb --enable-pcsc
+OPENCT_INSTALL_STAGING = YES
 OPENCT_AUTORECONF = YES
-#OPENCT_AUTORECONF_OPTS= --verbose --install
+
 
 $(eval $(autotools-package))
