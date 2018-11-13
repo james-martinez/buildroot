96d26fe14b36dfe95d29621641d6203dd38d6c93
James <james-martinez@users.noreply.github.com>
2018-11-13 11:14:44 -0600

    added headers to staging for paho

--- package/paho-mqtt-c/paho-mqtt-c.mk
+++ package/paho-mqtt-c/paho-mqtt-c.mk
@@ -12,18 +12,22 @@ PAHO_MQTT_C_INSTALL_STAGING = YES
 PAHO_MQTT_C_DEPENDENCIES += openssl
 PAHO_MQTT_C_CONF_OPTS += -DPAHO_WITH_SSL=TRUE  -DPAHO_BUILD_DOCUMENTATION=FALSE
 
-
 define PAHO_MQTT_C_BUILD_CMDS
-     $(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)
+     $(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D) build
  endef
 
 define PAHO_MQTT_C_INSTALL_STAGING_CMDS
-     $(INSTALL) -D -m 0755 $(@D)/build/output/libpaho-*.so* $(STAGING_DIR)/usr/lib
-     $(INSTALL) -D -m 0755 $(@D)/src/*.h $(STAGING_DIR)/usr/include
+	$(INSTALL) -D -m 0644 $(@D)/src/MQTTAsync.h $(STAGING_DIR)/usr/include/
+	$(INSTALL) -D -m 0644 $(@D)/src/MQTTClient.h $(STAGING_DIR)/usr/include/
+	$(INSTALL) -D -m 0644 $(@D)/src/MQTTClientPersistence.h $(STAGING_DIR)/usr/include/
+	$(INSTALL) -D -m 0644 $(@D)/src/MQTTProperties.h $(STAGING_DIR)/usr/include/
+	$(INSTALL) -D -m 0644 $(@D)/src/MQTTReasonCodes.h $(STAGING_DIR)/usr/include/
+	$(INSTALL) -D -m 0644 $(@D)/src/MQTTSubscribeOpts.h $(STAGING_DIR)/usr/include/
+        cp -a $(@D)/build/output/libpaho-mqtt*.so* $(STAGING_DIR)/usr/lib/
 endef
 
 define PAHO_MQTT_C_INSTALL_TARGET_CMDS
-      $(INSTALL) -D -m 0755 $(@D)/build/output/libpaho-*.so* $(TARGET_DIR)/usr/lib
+        cp -a $(@D)/build/output/libpaho-mqtt*.so* $(STAGING_DIR)/usr/lib/
  endef
 
 
