3db20a588cbece280455783da34c4b379d2420c2
James <james-martinez@users.noreply.github.com>
2018-11-13 14:52:11 -0600

    updated libusb and fixed conf opts in make script

--- package/openct/openct.mk
+++ package/openct/openct.mk
@@ -8,7 +8,7 @@ OPENCT_SITE = $(call github,OpenSC,openct,openct-$(OPENCT_VERSION))
 OPENCT_LICENSE = LGPL-2.1
 OPENCT_LICENSE_FILES = LGPL-2.1
 OPENCT_DEPENDENCIES = host-pkgconf libtool libusb-compat pcsc-lite
-OPENCT_CONF_OPT +=  --enable-usb --enable-pcsc
+OPENCT_CONF_OPTS +=  --enable-usb --enable-pcsc
 OPENCT_INSTALL_STAGING = YES
 OPENCT_AUTORECONF = YES
 
--- package/opensc/opensc.mk
+++ package/opensc/opensc.mk
@@ -9,30 +9,30 @@ OPENSC_LICENSE = LGPL2
 OPENSC_LICENSE_FILES = COPYING
 OPENSC_INSTALL_STAGING = YES
 OPENSC_DEPENDENCIES = host-pkgconf pcsc-lite
-OPENSC_CONF_OPT += --disable-strict
+OPENSC_CONF_OPTS += --disable-strict
 #OPENSC_AUTORECONF_OPTS= 
 OPENSC_AUTORECONF = YES
 
 
 #ifeq ($(BR2_PACKAGE_HAS_OPENSSL),y)
-OPENSC_CONF_OPT += --enable-openssl
+OPENSC_CONF_OPTS += --enable-openssl
 OPENSC_DEPENDENCIES += openssl
 #else
-#OPENSC_CONF_OPT += --disable-openssl
+#OPENSC_CONF_OPTS += --disable-openssl
 #endif
 
 ifeq ($(BR2_PACKAGE_HAS_OPENPACE),y)
-OPENSC_CONF_OPT += --enable-openssl
+OPENSC_CONF_OPTS += --enable-openssl
 OPENSC_DEPENDENCIES += openpace
 else
-OPENSC_CONF_OPT += --disable-openpace
+OPENSC_CONF_OPTS += --disable-openpace
 endif
 
 ifeq ($(BR2_PACKAGE_HAS_READLINE),y)
-OPENSC_CONF_OPT += --enable-readline
+OPENSC_CONF_OPTS += --enable-readline
 OPENSC_DEPENDENCIES += readline
 else
-OPENSC_CONF_OPT += --disable-readline
+OPENSC_CONF_OPTS += --disable-readline
 endif
 
 $(eval $(autotools-package))
