d2b80a927864ee8e2828c9dc44f6d45d04709a0e
James <james-martinez@users.noreply.github.com>
2018-11-13 14:30:01 -0600

    added mqtt and smart card com

---
+++ package/opensc/opensc.mk
@@ -0,0 +1,38 @@
+################################################################################
+#
+# OpenSC-0.19.0
+################################################################################
+
+OPENSC_VERSION =0.19.0
+OPENSC_SITE = $(call github,OpenSC,OpenSC,$(OPENSC_VERSION))
+OPENSC_LICENSE = LGPL2
+OPENSC_LICENSE_FILES = COPYING
+OPENSC_INSTALL_STAGING = YES
+OPENSC_DEPENDENCIES = host-pkgconf pcsc-lite
+OPENSC_CONF_OPT += --disable-strict
+#OPENSC_AUTORECONF_OPTS= 
+OPENSC_AUTORECONF = YES
+
+
+#ifeq ($(BR2_PACKAGE_HAS_OPENSSL),y)
+OPENSC_CONF_OPT += --enable-openssl
+OPENSC_DEPENDENCIES += openssl
+#else
+#OPENSC_CONF_OPT += --disable-openssl
+#endif
+
+ifeq ($(BR2_PACKAGE_HAS_OPENPACE),y)
+OPENSC_CONF_OPT += --enable-openssl
+OPENSC_DEPENDENCIES += openpace
+else
+OPENSC_CONF_OPT += --disable-openpace
+endif
+
+ifeq ($(BR2_PACKAGE_HAS_READLINE),y)
+OPENSC_CONF_OPT += --enable-readline
+OPENSC_DEPENDENCIES += readline
+else
+OPENSC_CONF_OPT += --disable-readline
+endif
+
+$(eval $(autotools-package))
--- package/paho-mqtt-c/paho-mqtt-c.mk
+++ package/paho-mqtt-c/paho-mqtt-c.mk
@@ -4,17 +4,31 @@
 #
 ################################################################################
 
-PAHO_MQTT_C_VERSION = v1.2.0
+PAHO_MQTT_C_VERSION = v1.3.0
 PAHO_MQTT_C_SITE = $(call github,eclipse,paho.mqtt.c,$(PAHO_MQTT_C_VERSION))
 PAHO_MQTT_C_LICENSE = EPL-1.0 or BSD-3-Clause
 PAHO_MQTT_C_LICENSE_FILES = epl-v10 edl-v10
 PAHO_MQTT_C_INSTALL_STAGING = YES
-
-ifeq ($(BR2_PACKAGE_OPENSSL),y)
 PAHO_MQTT_C_DEPENDENCIES += openssl
-PAHO_MQTT_C_CONF_OPTS += -DPAHO_WITH_SSL=TRUE
-else
-PAHO_MQTT_C_CONF_OPTS += -DPAHO_WITH_SSL=FALSE
-endif
+PAHO_MQTT_C_CONF_OPTS += -DPAHO_WITH_SSL=TRUE  -DPAHO_BUILD_DOCUMENTATION=FALSE
 
-$(eval $(cmake-package))
+define PAHO_MQTT_C_BUILD_CMDS
+     $(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D) build
+ endef
+
+define PAHO_MQTT_C_INSTALL_STAGING_CMDS
+	$(INSTALL) -D -m 0644 $(@D)/src/MQTTAsync.h $(STAGING_DIR)/usr/include/
+	$(INSTALL) -D -m 0644 $(@D)/src/MQTTClient.h $(STAGING_DIR)/usr/include/
+	$(INSTALL) -D -m 0644 $(@D)/src/MQTTClientPersistence.h $(STAGING_DIR)/usr/include/
+	$(INSTALL) -D -m 0644 $(@D)/src/MQTTProperties.h $(STAGING_DIR)/usr/include/
+	$(INSTALL) -D -m 0644 $(@D)/src/MQTTReasonCodes.h $(STAGING_DIR)/usr/include/
+	$(INSTALL) -D -m 0644 $(@D)/src/MQTTSubscribeOpts.h $(STAGING_DIR)/usr/include/
+        cp -a $(@D)/build/output/libpaho-mqtt*.so* $(STAGING_DIR)/usr/lib/
+endef
+
+define PAHO_MQTT_C_INSTALL_TARGET_CMDS
+        cp -a $(@D)/build/output/libpaho-mqtt*.so* $(STAGING_DIR)/usr/lib/
+ endef
+
+
+$(eval $(generic-package))
