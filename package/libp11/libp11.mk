7d27e97055e95e7fc53fea73fbf27cc173aa3a65
James <james-martinez@users.noreply.github.com>
2018-11-19 17:10:25 -0600

    added libp11

---
+++ package/libp11/libp11.mk
@@ -0,0 +1,15 @@
+################################################################################
+#
+# libp11-0.4.9
+################################################################################
+
+LIBP11_VERSION =0.4.9
+LIBP11_SITE = https://github.com/OpenSC/libp11/releases/download/libp11-$(LIBP11_VERSION)
+LIBP11_LICENSE = LGPL-2.1
+LIBP11_LICENSE_FILES = COPYING
+LIBP11_INSTALL_STAGING = YES
+LIBP11_DEPENDENCIES =openssl
+LIBP11_CONF_OPTS += --enable-openssl
+LIBP11_AUTORECONF = YES
+
+$(eval $(autotools-package))
