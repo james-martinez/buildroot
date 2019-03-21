cc76cb38e67ce5dd67ea9de731f4dcaa7f7ede33
James <james-martinez@users.noreply.github.com>
2019-03-21 17:12:35 -0500

    updated wpa_supplicant and bluez5_utils, made boot archive gzipped

--- board/raspberrypi/lancer/rpi/genboottar.sh
+++ board/raspberrypi/lancer/rpi/genboottar.sh
@@ -2,7 +2,7 @@
 
 CUR_DIR=$(cd `dirname $0` && pwd)
 echo "Creating boot tar"
-tar -cf $BINARIES_DIR/boot.tar  --numeric-owner \
+tar -czf $BINARIES_DIR/boot.tar.gz  --numeric-owner \
 -C  $BINARIES_DIR zImage bcm2710-rpi-3-b.dtb bcm2710-rpi-3-b-plus.dtb \
 -C  $BINARIES_DIR/rpi-firmware bootcode.bin  start.elf fixup.dat overlays/ \
 -C  $CUR_DIR cmdline.txt config.txt
