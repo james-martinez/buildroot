5919b715e4acecdaba651271e922ce8e9edbcf93
James <james-martinez@users.noreply.github.com>
2019-04-22 16:37:43 -0500

    added features for openct to select hadware provider, updaded p11-kit , added pkcs11-helper

--- package/openct/openct.mk
+++ package/openct/openct.mk
@@ -7,8 +7,8 @@ OPENCT_VERSION =0.6.20
 OPENCT_SITE = $(call github,OpenSC,openct,openct-$(OPENCT_VERSION))
 OPENCT_LICENSE = LGPL-2.1
 OPENCT_LICENSE_FILES = LGPL-2.1
-OPENCT_DEPENDENCIES = host-pkgconf libtool libusb-compat pcsc-lite
-OPENCT_CONF_OPT +=  --enable-usb --enable-pcsc
+OPENCT_DEPENDENCIES = host-pkgconf libtool libusb-compat
+OPENCT_CONF_OPT +=  --enable-usb
 OPENCT_INSTALL_STAGING = YES
 OPENCT_AUTORECONF = YES
 
--- package/opensc/opensc.mk
+++ package/opensc/opensc.mk
@@ -8,18 +8,17 @@ OPENSC_SITE = $(call github,OpenSC,OpenSC,$(OPENSC_VERSION))
 OPENSC_LICENSE = LGPL2
 OPENSC_LICENSE_FILES = COPYING
 OPENSC_INSTALL_STAGING = YES
-OPENSC_DEPENDENCIES = host-pkgconf pcsc-lite
-OPENSC_CONF_OPT += --disable-strict
-#OPENSC_AUTORECONF_OPTS= 
+OPENSC_DEPENDENCIES = host-pkgconf 
+OPENSC_CONF_OPT += --disable-strict 
 OPENSC_AUTORECONF = YES
 
 
-#ifeq ($(BR2_PACKAGE_HAS_OPENSSL),y)
+ifeq ($(BR2_PACKAGE_HAS_OPENSSL),y)
 OPENSC_CONF_OPT += --enable-openssl
 OPENSC_DEPENDENCIES += openssl
-#else
-#OPENSC_CONF_OPT += --disable-openssl
-#endif
+else
+OPENSC_CONF_OPT += --disable-openssl
+endif
 
 ifeq ($(BR2_PACKAGE_HAS_OPENPACE),y)
 OPENSC_CONF_OPT += --enable-openssl
@@ -33,6 +32,20 @@ OPENSC_CONF_OPT += --enable-readline
 OPENSC_DEPENDENCIES += readline
 else
 OPENSC_CONF_OPT += --disable-readline
+endif
+
+ifeq ($(BR2_PACKAGE_OPENCT),y)
+OPENSC_CONF_OPT += --enable-openct
+OPENSC_DEPENDENCIES += openct
+else
+OPENSC_CONF_OPT += --disable-openct
+endif
+
+ifeq ($(BR2_PACKAGE_PCSC_LITE),y)
+OPENSC_CONF_OPT += --enable-pcsc
+OPENSC_DEPENDENCIES += pcsc-lite
+else
+OPENSC_CONF_OPT += --disable-pcsc
 endif
 
 $(eval $(autotools-package))
