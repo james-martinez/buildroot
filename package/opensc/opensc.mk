6794a3f7601035ae28c044b6b00611f6078a3ee0
James <james-martinez@users.noreply.github.com>
2019-04-23 11:00:58 -0500

    fixed opensc to select openct or pcsc-lite with ccid

--- package/opensc/opensc.mk
+++ package/opensc/opensc.mk
@@ -10,29 +10,41 @@ OPENSC_LICENSE_FILES = COPYING
 OPENSC_INSTALL_STAGING = YES
 OPENSC_DEPENDENCIES = host-pkgconf pcsc-lite
 OPENSC_CONF_OPTS += --disable-strict
-#OPENSC_AUTORECONF_OPTS= 
 OPENSC_AUTORECONF = YES
 
-
-#ifeq ($(BR2_PACKAGE_HAS_OPENSSL),y)
-OPENSC_CONF_OPTS += --enable-openssl
+ifeq ($(BR2_PACKAGE_HAS_OPENSSL),y)
+OPENSC_CONF_OPT += --enable-openssl
 OPENSC_DEPENDENCIES += openssl
-#else
-#OPENSC_CONF_OPTS += --disable-openssl
-#endif
+else
+OPENSC_CONF_OPT += --disable-openssl
+endif
 
 ifeq ($(BR2_PACKAGE_HAS_OPENPACE),y)
-OPENSC_CONF_OPTS += --enable-openssl
+OPENSC_CONF_OPT += --enable-openssl
 OPENSC_DEPENDENCIES += openpace
 else
-OPENSC_CONF_OPTS += --disable-openpace
+OPENSC_CONF_OPT += --disable-openpace
 endif
 
 ifeq ($(BR2_PACKAGE_HAS_READLINE),y)
-OPENSC_CONF_OPTS += --enable-readline
+OPENSC_CONF_OPT += --enable-readline
 OPENSC_DEPENDENCIES += readline
 else
-OPENSC_CONF_OPTS += --disable-readline
+OPENSC_CONF_OPT += --disable-readline
+endif
+
+ifeq ($(BR2_PACKAGE_OPENCT),y)
+OPENSC_CONF_OPT += --enable-openct
+OPENSC_DEPENDENCIES += openct
+else
+OPENSC_CONF_OPT += --disable-openct
+endif
+
+ifeq ($(BR2_PACKAGE_PCSC_LITE),y)
+OPENSC_CONF_OPT += --enable-pcsc
+OPENSC_DEPENDENCIES += pcsc-lite
+else
+OPENSC_CONF_OPT += --disable-pcsc
 endif
 
 $(eval $(autotools-package))
---
+++ package/pkcs11-helper/pkcs11-helper.mk
@@ -0,0 +1,15 @@
+################################################################################
+#
+# pkcs11-helper
+#
+################################################################################
+
+PKCS11_HELPER_VERSION = 1.25.1
+PKCS11_HELPER_SITE = $(call github,OpenSC,pkcs11-helper,pkcs11-helper-$(PKCS11_HELPER_VERSION))
+PKCS11_HELPER_DEPENDENCIES = host-pkgconf
+PKCS11_HELPER_INSTALL_STAGING = YES
+PKCS11_HELPER_CONF_OPT = --disable-static
+PKCS11_HELPER_LICENSE = GPL2
+PKCS11_HELPER_LICENSE_FILES = COPYING
+PKCS11_HELPER_AUTORECONF = YES
+$(eval $(autotools-package))
