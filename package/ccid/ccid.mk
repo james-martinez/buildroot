a186c4013798db58c16032693cfcb5a9e699a9a5
James <james-martinez@users.noreply.github.com>
2019-05-03 11:59:00 -0500

    fixed conf options and dependecies on ccid

--- package/ccid/ccid.mk
+++ package/ccid/ccid.mk
@@ -10,7 +10,7 @@ CCID_SITE = https://ccid.apdu.fr/files
 CCID_LICENSE = LGPL-2.1+
 CCID_LICENSE_FILES = COPYING
 CCID_INSTALL_STAGING = YES
-CCID_DEPENDENCIES = pcsc-lite host-pkgconf libusb
+CCID_DEPENDENCIES = host-pkgconf libusb
 
 ifeq ($(BR2_PACKAGE_HAS_UDEV),y)
 define CCID_INSTALL_UDEV_RULES
--- package/opensc/opensc.mk
+++ package/opensc/opensc.mk
@@ -13,38 +13,38 @@ OPENSC_CONF_OPTS += --disable-strict
 OPENSC_AUTORECONF = YES
 
 ifeq ($(BR2_PACKAGE_HAS_OPENSSL),y)
-OPENSC_CONF_OPT += --enable-openssl
+OPENSC_CONF_OPTS += --enable-openssl
 OPENSC_DEPENDENCIES += openssl
 else
-OPENSC_CONF_OPT += --disable-openssl
+OPENSC_CONF_OPTS += --disable-openssl
 endif
 
 ifeq ($(BR2_PACKAGE_HAS_OPENPACE),y)
-OPENSC_CONF_OPT += --enable-openssl
+OPENSC_CONF_OPTS += --enable-openssl
 OPENSC_DEPENDENCIES += openpace
 else
-OPENSC_CONF_OPT += --disable-openpace
+OPENSC_CONF_OPTS += --disable-openpace
 endif
 
 ifeq ($(BR2_PACKAGE_HAS_READLINE),y)
-OPENSC_CONF_OPT += --enable-readline
+OPENSC_CONF_OPTS += --enable-readline
 OPENSC_DEPENDENCIES += readline
 else
-OPENSC_CONF_OPT += --disable-readline
+OPENSC_CONF_OPTS += --disable-readline
 endif
 
 ifeq ($(BR2_PACKAGE_OPENCT),y)
-OPENSC_CONF_OPT += --enable-openct
+OPENSC_CONF_OPTS += --enable-openct
 OPENSC_DEPENDENCIES += openct
 else
-OPENSC_CONF_OPT += --disable-openct
+OPENSC_CONF_OPTS += --disable-openct
 endif
 
 ifeq ($(BR2_PACKAGE_PCSC_LITE),y)
-OPENSC_CONF_OPT += --enable-pcsc
+OPENSC_CONF_OPTS += --enable-pcsc
 OPENSC_DEPENDENCIES += pcsc-lite
 else
-OPENSC_CONF_OPT += --disable-pcsc
+OPENSC_CONF_OPTS += --disable-pcsc
 endif
 
 $(eval $(autotools-package))
